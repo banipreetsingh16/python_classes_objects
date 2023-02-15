b72f1d7aefa1cd8cc6ca2215ee316761ab4ea9c3
banipreetsingh16 <123930274+banipreetsingh16@users.noreply.github.com>
2023-02-15 10:53:39 +0530

    Update class_objects.py

--- class_objects.py
+++ class_objects.py
@@ -1,69 +1,69 @@
 #first Example
-class motorcycle():
+class Motorcycle:
     wheels = 2
     engine = "small"
     def speed(self):
         print("Motorcycles these days are really fast")
-bullet = motorcycle()
+bullet = Motorcycle()
 bullet.speed()
-print(motorcycle.wheels)
-print(motorcycle.engine)
-motorcycle.speed(bullet)
+print(Motorcycle.wheels)
+print(Motorcycle.engine)
+Motorcycle.speed(bullet)
 print("------------------------------------------------------")
 
 
 #second example
-class states():
+class States:
     Num_of_States=28
     Area_covered=3,287,263
     def diversity(self):
         print("India is a very diverse country!")
-Punjab = states()
+Punjab = States()
 Punjab.diversity()
-print(states.Num_of_States)
-print(states.Area_covered)
-states.diversity(Punjab)
+print(States.Num_of_States)
+print(States.Area_covered)
+States.diversity(Punjab)
 print("------------------------------------------------------")
 
 
 # Third Example
-class dogs():
+class Dogs:
     legs = 4
     tail = "yes"
     def bark(self):
         print("Dog barks loudly")
-Saint_Brenard = dogs()
-print(dogs.legs)
+Saint_Brenard = Dogs()
+print(Dogs.legs)
 Saint_Brenard.bark()
-dogs.bark(Saint_Brenard)
-print(dogs.tail)
+Dogs.bark(Saint_Brenard)
+print(Dogs.tail)
 print("------------------------------------------------------")
 
 
 # Fourth Example
-class hotel():
+class Hotel:
     floors=5
     rooms=30
     def size(self):
         print("This hotel is really big!!!")
-TheTaj = hotel()
+TheTaj = Hotel()
 TheTaj.size()
-print(hotel.floors)
-print(hotel.rooms)
-hotel.size(TheTaj)
+print(Hotel.floors)
+print(Hotel.rooms)
+Hotel.size(TheTaj)
 print("------------------------------------------------------")
 
 
 # Fifth Example'
-class laptops():
+class Laptops:
     handy = "yes"
     size = "small"
     def use(self):
         print("Laptops are handy and we can use them anywhere")
-macbook= laptops()
-print(laptops.handy)
-print(laptops.size)
-laptops.use(macbook)
+macbook= Laptops()
+print(Laptops.handy)
+print(Laptops.size)
+Laptops.use(macbook)
 macbook.use()
 print("------------------------------------------------------")
 
